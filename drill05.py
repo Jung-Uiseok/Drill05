1c2cf27fafeb05e637f4850aa05099fd77c95d8f
Jung-Uiseok <tnekwjd3359@naver.com>
2023-10-01 18:14:27 +0900

    change hand_arrow location

--- drill05.py
+++ drill05.py
@@ -1,11 +1,12 @@
-from pico2d import*
+from pico2d import *
 import random
 
-open_canvas(1280,1080)
+open_canvas(1280, 1080)
 
 TUK_ground = load_image('TUK_GROUND.png')
 hand_arrow = load_image('hand_arrow.png')
 character = load_image('animation_sheet.png')
+
 
 def handle_events():
     global running
@@ -18,29 +19,36 @@ def handle_events():
             if event.key == SDLK_ESCAPE:
                 running = False
 
+points = [(random.randint(0, 1280), random.randint(0, 1080)) for i in range(10)]
+
 running = True
-x = random.randint(0,1280)
-y = random.randint(0,1080)
-ch_x = 1280 // 2
-ch_y = 1080 // 2
-frame = 0
 
 while running:
-    clear_canvas()
-    TUK_ground.draw(1280//2, 1080//2)
+    def draw_line(p1, p2):
+        x1, y1 = p1[0], p1[1]
+        x2, y2 = p2[0], p2[1]
+        frame = 0  # frame 변수 초기화
 
-    for i in range(0, 100+1, 4):
-        t = i / 100
-        ch_x = (1-t) * ch_x + t * x
-        ch_y = (1-t) * ch_y + t * y
+        for i in range(0, 100 + 1, 4):
+            t = i / 100
+            x = int((1 - t) * x1 + t * x2)
+            y = int((1 - t) * y1 + t * y2)
 
-        character.clip_draw(frame * 100, 100 * 1, 100, 100, ch_x, ch_y)
-        hand_arrow.draw(x,y)
+            clear_canvas()
+            TUK_ground.draw(1280 // 2, 1080 // 2)
 
-        update_canvas()
-        frame = (frame + 1) % 8
+            character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
+            hand_arrow.draw(x2, y2)
 
-        delay(0.02)
+            update_canvas()
+            frame = (frame + 1) % 8
+
+            delay(0.02)
+
+
+    for i in range(0, len(points) - 1):
+        draw_line(points[i], points[i + 1])
+    draw_line(points[-1], points[0])
 
     handle_events()
 
