17a699e2a7c5ed4431f0664635db8e10e6edef51
Jung-Uiseok <tnekwjd3359@naver.com>
2023-10-01 17:35:00 +0900

    character move to hand_arrow

--- drill05.py
+++ drill05.py
@@ -21,15 +21,26 @@ def handle_events():
 running = True
 x = random.randint(0,1280)
 y = random.randint(0,1080)
+ch_x = 1280 // 2
+ch_y = 1080 // 2
 frame = 0
 
 while running:
     clear_canvas()
     TUK_ground.draw(1280//2, 1080//2)
-    #character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
-    hand_arrow.draw(x,y)
-    update_canvas()
-    frame = (frame + 1) % 8
+
+    for i in range(0, 100+1, 4):
+        t = i / 100
+        ch_x = (1-t) * ch_x + t * x
+        ch_y = (1-t) * ch_y + t * y
+
+        character.clip_draw(frame * 100, 100 * 1, 100, 100, ch_x, ch_y)
+        hand_arrow.draw(x,y)
+
+        update_canvas()
+        frame = (frame + 1) % 8
+
+        delay(0.02)
 
     handle_events()
 
