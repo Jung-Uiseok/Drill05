b670833bd5a6955cefff5dca70627f0bbc7aecb8
Jung-Uiseok <tnekwjd3359@naver.com>
2023-10-01 20:30:30 +0900

    fix size of canvas

--- drill05.py
+++ drill05.py
@@ -1,7 +1,7 @@
 from pico2d import *
 import random
 
-open_canvas(1280, 1080)
+open_canvas(1280, 1024)
 
 TUK_ground = load_image('TUK_GROUND.png')
 hand_arrow = load_image('hand_arrow.png')
@@ -19,36 +19,39 @@ def handle_events():
             if event.key == SDLK_ESCAPE:
                 running = False
 
-points = [(random.randint(0, 1280), random.randint(0, 1080)) for i in range(10)]
+points = [(random.randint(0, 1280), random.randint(0, 1024)) for i in range(10)]
 
 running = True
+frame = 0
+
+def draw_line(p1, p2):
+    global frame
+
+    x1, y1 = p1[0], p1[1]
+    x2, y2 = p2[0], p2[1]
+
+    for i in range(0, 100 + 1, 4):
+        t = i / 100
+        x = (1 - t) * x1 + t * x2
+        y = (1 - t) * y1 + t * y2
+
+        clear_canvas()
+        TUK_ground.draw(1280 // 2, 1024 // 2)
+
+        if x1 > x2:
+            character.clip_composite_draw(frame * 100, 100 * 1, 100, 100, 0, 'h', x, y, 200, 200)
+        else:
+            character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y, 200, 200)
+
+        hand_arrow.draw(x2, y2)
+
+        update_canvas()
+        frame = (frame + 1) % 8
+
+        delay(0.02)
+
 
 while running:
-    def draw_line(p1, p2):
-        x1, y1 = p1[0], p1[1]
-        x2, y2 = p2[0], p2[1]
-        frame = 0  # frame 변수 초기화
-
-        for i in range(0, 100 + 1, 4):
-            t = i / 100
-            x = int((1 - t) * x1 + t * x2)
-            y = int((1 - t) * y1 + t * y2)
-
-            clear_canvas()
-            TUK_ground.draw(1280 // 2, 1080 // 2)
-
-            if x1 > x2:
-                character.clip_composite_draw(frame * 100, 100 * 1, 100, 100, 0, 'h', x, y, 200, 200)
-            else:
-                character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y, 200, 200)
-
-            hand_arrow.draw(x2, y2)
-
-            update_canvas()
-            frame = (frame + 1) % 8
-
-            delay(0.02)
-
 
     for i in range(0, len(points) - 1):
         draw_line(points[i], points[i + 1])
